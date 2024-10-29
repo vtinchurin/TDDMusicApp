7e546259ec7b0b3964c635213e9c2020650d0824
vtinchurin <vladislav.tinchurin@gmail.com>
2024-10-29 10:00:21 +0300

    MA-11 SearchUiState

---
+++ app/src/main/java/com/ru/androidexperts/muzicapp/SearchUiState.kt
@@ -0,0 +1,44 @@
+package com.ru.androidexperts.muzicapp
+
+import com.ru.androidexperts.muzicapp.adapter.GenericAdapter
+import com.ru.androidexperts.muzicapp.adapter.RecyclerItem
+import com.ru.androidexperts.muzicapp.view.UpdateText
+
+interface SearchUiState {
+
+    fun show(
+        input: UpdateText,
+        adapter: GenericAdapter,
+    )
+
+    fun recyclerState(): List<RecyclerItem>
+
+    abstract class Abstract(
+        private val recyclerState: List<RecyclerItem>,
+    ) : SearchUiState {
+
+        override fun show(input: UpdateText, adapter: GenericAdapter) {
+            adapter.update(recyclerState)
+        }
+
+        override fun recyclerState() = recyclerState
+    }
+
+    data class Initial(private val inputText: String) : Abstract(recyclerState = listOf()) {
+
+        override fun show(input: UpdateText, adapter: GenericAdapter) {
+            input.update(inputText)
+        }
+    }
+
+    data class Success(
+        private val recyclerState: List<RecyclerItem>,
+    ) : Abstract(recyclerState = recyclerState)
+
+    data class Error(private val message: String) :
+        Abstract(recyclerState = listOf(RecyclerItem.ErrorUi(message)))
+
+    object Loading : Abstract(recyclerState = listOf(RecyclerItem.ProgressUi))
+
+    object NoTracks : Abstract(recyclerState = listOf(RecyclerItem.NoTracksUi))
+}
