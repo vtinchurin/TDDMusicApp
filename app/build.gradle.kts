96bd7564586a576f06632305b58b9f2e2da86870
Vladislav Tinchurin <161710338+vtinchurin@users.noreply.github.com>
2024-11-06 14:26:29 +0300

    MA-12-music-player (#19)
    * MA-12 add media3 exoplayer dependencies
    
    * MA-12 add ExoPlayer wrapper
    
    * MA-12 compare solution by Danilov
    
    * MA-12 compare solution by Danilov
    
    * Add Playlist (#20)
    
    * Add Playlist
    
    * Refactor callback
    
    * fix updateCallback
    
    * delete playlist (#21)
    
    * MA-12 add more complex behavior in player logic
    
    * MA-12 rework player
    
    * Merge Feature/ma 12 music player add playlist into MA-12 (#24)
    
    * Add Playlist
    
    * Refactor callback
    
    * fix updateCallback
    
    * Player that work with playlist by id and uri. Handling all actions
    
    ---------
    
    Co-authored-by: Malevichr <144087226+Malevichr@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.ui)
 }
