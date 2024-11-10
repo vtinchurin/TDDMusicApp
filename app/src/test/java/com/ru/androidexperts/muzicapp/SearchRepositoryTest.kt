99086b904883d43d4efe340b5478b3aab1dbe2a8
vtinchurin <vladislav.tinchurin@gmail.com>
2024-11-10 22:20:54 +0300

    MA-15 add TrackModel and runBlocking in tests

--- app/src/test/java/com/ru/androidexperts/muzicapp/SearchRepositoryTest.kt
+++ app/src/test/java/com/ru/androidexperts/muzicapp/SearchRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.ru.androidexperts.muzicapp
 
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
@@ -27,13 +28,13 @@ class SearchRepositoryTest {
     }
 
     @Test
-    fun `not cached data load`() {
-        val tracks = repository.load(term = "not_cached")
+    fun `not cached data load`(): Unit = runBlocking {
+        val tracks: List<TrackModel> = repository.load(term = "not_cached")
         cacheDataSource.assertContainsCalledCount(count = 1)
         cloudDataSource.assertLoadCalledCount(count = 1)
         cacheDataSource.assertSaveCalledCount(count = 1)
         cacheDataSource.assertResult(CLOUD_AND_CACHED_TRACKS)
-        assertEquals(tracks, CLOUD_AND_CACHED_TRACKS)
+        assertEquals(tracks, LOAD_RESULT)
         cachedTerm.assertValue("not_cached")
         order.assert(
             listOf(
@@ -47,13 +48,13 @@ class SearchRepositoryTest {
     }
 
     @Test
-    fun `cached data load`() {
-        val tracks = repository.load(term = "cached")
+    fun `cached data load`(): Unit = runBlocking {
+        val tracks: List<TrackModel> = repository.load(term = "cached")
         cacheDataSource.assertContainsCalledCount(count = 1)
         cloudDataSource.assertLoadCalledCount(count = 0)
         cacheDataSource.assertSaveCalledCount(count = 0)
         cacheDataSource.assertResult(CACHED_TRACKS)
-        assertEquals(tracks, CACHED_TRACKS)
+        assertEquals(tracks, LOAD_RESULT)
         cachedTerm.assertValue("cached")
         order.assert(
             listOf(
@@ -90,6 +91,22 @@ class SearchRepositoryTest {
                 sourceUrl = "sourceUrl 3"
             ),
             TrackCache(
+                id = 4L,
+                trackTitle = "title 4",
+                authorName = "author 4",
+                coverUrl = "coverUrl 4",
+                sourceUrl = "sourceUrl 4"
+            )
+        )
+        private val LOAD_RESULT: List<TrackModel> = listOf(
+            TrackModel(
+                id = 3L,
+                trackTitle = "title 3",
+                authorName = "author 3",
+                coverUrl = "coverUrl 3",
+                sourceUrl = "sourceUrl 3"
+            ),
+            TrackModel(
                 id = 4L,
                 trackTitle = "title 4",
                 authorName = "author 4",
