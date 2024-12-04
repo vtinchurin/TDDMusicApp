ee0b3a1d1aa5e907386265606db9ea7329df772f
Malevichr <144087226+Malevichr@users.noreply.github.com>
2024-12-04 09:55:10 +0300

    MA-04 fix CustomViews and add Picasso (#35)
    * MA-04 fix InputView and PlayStopUiState
    
    * MA-04 add update by textResId
    
    * MA-04 add Picasso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.exoplayer.dash)
     implementation(libs.androidx.media3.ui)
+
+    implementation (libs.picasso)
 }
