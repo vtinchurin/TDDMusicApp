a152f44e96bc915e27f3f6b3880209d8f116ecee
Vladislav Tinchurin <161710338+vtinchurin@users.noreply.github.com>
2024-12-04 09:39:53 +0300

    MA-10-ui-observable (#16)
    * MA-10 UiObservable
    
    * MA-10 add TODO
    
    * MA-10 change method playNext to nextTrack
    
    * MA-10 change method play signature, and delete useless method
    
    * MA-10 add change previous played track to StopState before play current
    
    * MA-10 add scope functions and change type of saved value for currently played track
    
    * MA-10 add some changes with scopeFun's

---
+++ app/src/main/java/com/ru/androidexperts/muzicapp/uiObservable/UiObservable.kt
@@ -0,0 +1,61 @@
+package com.ru.androidexperts.muzicapp.uiObservable
+
+import com.ru.androidexperts.muzicapp.SearchUiState
+
+interface UiObservable<T : Any> : Update<T> {
+
+    fun update(observer: UiObserver<T> = UiObserver.Empty())
+
+    interface Playlist<T : Any> : UiObservable<T> {
+
+        fun play(trackId: Long)
+
+        fun stop()
+
+        class Base : Playlist<SearchUiState> {
+
+            private var cached: SearchUiState = SearchUiState.Initial("")
+            private var observer: UiObserver<SearchUiState> = UiObserver.Empty()
+
+            override fun update(observer: UiObserver<SearchUiState>) {
+                this.observer = observer
+                if (!observer.isEmpty())
+                    observer.updateUi(cached)
+            }
+
+            override fun updateUi(data: SearchUiState) {
+                if (!observer.isEmpty())
+                    observer.updateUi(data)
+                cached = data
+            }
+
+            override fun play(trackId: Long) {
+                val tracks = cached.recyclerState().toMutableList()
+                tracks.find {
+                    it.isPlaying()
+                }?.let {
+                    val index = tracks.indexOf(it)
+                    tracks[index] = it.changePlaying()
+                }
+                tracks.find {
+                    it.trackId() == trackId
+                }?.let {
+                    val index = tracks.indexOf(it)
+                    tracks[index] = it.changePlaying()
+                }
+                updateUi(SearchUiState.Success(recyclerState = tracks))
+            }
+
+            override fun stop() {
+                val tracks = cached.recyclerState().toMutableList()
+                tracks.find {
+                    it.isPlaying()
+                }?.let {
+                    val index = tracks.indexOf(it)
+                    tracks[index] = it.changePlaying()
+                }
+                updateUi(SearchUiState.Success(recyclerState = tracks))
+            }
+        }
+    }
+}
