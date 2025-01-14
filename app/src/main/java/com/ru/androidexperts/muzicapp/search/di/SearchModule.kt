10e0c07267e83ca42612807e376a828659a548c5
vtinchurin <vladislav.tinchurin@gmail.com>
2025-01-14 16:13:00 +0300

    fix-39: Add CloudModule

--- app/src/main/java/com/ru/androidexperts/muzicapp/search/di/SearchModule.kt
+++ app/src/main/java/com/ru/androidexperts/muzicapp/search/di/SearchModule.kt
@@ -9,42 +9,21 @@ import com.ru.androidexperts.muzicapp.di.Module
 import com.ru.androidexperts.muzicapp.search.data.DataException
 import com.ru.androidexperts.muzicapp.search.data.cache.CacheDataSource
 import com.ru.androidexperts.muzicapp.search.data.cloud.CloudDataSource
-import com.ru.androidexperts.muzicapp.search.data.cloud.TrackService
 import com.ru.androidexperts.muzicapp.search.data.repository.SearchRepositoryBase
 import com.ru.androidexperts.muzicapp.search.presentation.SearchViewModel
 import com.ru.androidexperts.muzicapp.search.presentation.mappers.PlayerMapper
 import com.ru.androidexperts.muzicapp.search.presentation.mappers.UiMapper
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 class SearchModule(private val core: Core) : Module<SearchViewModel> {
 
     override fun viewModel(): SearchViewModel {
-        val client = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                setLevel(HttpLoggingInterceptor.Level.BODY)
-            })
-            .readTimeout(HTTP_CLIENT_READ_TIMEOUT, TimeUnit.SECONDS)
-            .writeTimeout(HTTP_CLIENT_WRITE_TIMEOUT, TimeUnit.SECONDS)
-            .connectTimeout(HTTP_CLIENT_CONNECT_TIMEOUT, TimeUnit.SECONDS)
-            .retryOnConnectionFailure(true)
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(API_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
         val repository = SearchRepositoryBase(
             cacheDataSource = CacheDataSource.Base(
                 dao = core.cacheModule.dao()
             ),
             cloudDataSource = CloudDataSource.Base(
-                service = retrofit.create(TrackService::class.java)
+                service = core.cloudModule.service()
             ),
             handleError = HandleError.ToData(),
             errorLoadResult = DataException.Mapper.ToErrorLoadResult(),
@@ -63,13 +42,5 @@ class SearchModule(private val core: Core) : Module<SearchViewModel> {
             toUi = UiMapper.Base(),
             toPlayList = PlayerMapper.Base()
         )
-    }
-
-    companion object {
-
-        private const val API_URL = "https://itunes.apple.com/"
-        private const val HTTP_CLIENT_READ_TIMEOUT = 60L
-        private const val HTTP_CLIENT_WRITE_TIMEOUT = 60L
-        private const val HTTP_CLIENT_CONNECT_TIMEOUT = 60L
     }
 }
